73fdb1d8350087c049e982086b7cbecf5c461335
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-03-26 11:24:04 +0700

    Revert "Turn on view binding"
    This reverts commit 1a4b0fe74761432dbd596aa96e7a1a2fa98984c0.

--- app/src/main/java/com/nastirlex/cinema/MainActivity.kt
+++ app/src/main/java/com/nastirlex/cinema/MainActivity.kt
@@ -2,16 +2,10 @@ package com.nastirlex.cinema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.nastirlex.cinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
     }
 }
