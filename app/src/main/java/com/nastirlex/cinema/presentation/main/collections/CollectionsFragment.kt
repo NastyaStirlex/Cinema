587c083fcc80332baeea3523579a45ffbb3330d4
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-03-31 22:39:31 +0700

    Collections fragment layout
    - not clickable rows

---
+++ app/src/main/java/com/nastirlex/cinema/presentation/main/collections/CollectionsFragment.kt
@@ -0,0 +1,53 @@
+package com.nastirlex.cinema.presentation.main.collections
+
+import android.content.Context
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nastirlex.cinema.databinding.FragmentCollectionsBinding
+import com.nastirlex.cinema.utils.SpacesItemDecoration
+
+class CollectionsFragment : Fragment() {
+    private lateinit var _binding: FragmentCollectionsBinding
+
+    private val binding get() = _binding
+
+    fun Context.dpToPixel(dp: Float): Float {
+        return dp * (resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCollectionsBinding.inflate(inflater, container, false)
+
+        _binding.collectionsRecyclerView.layoutManager =
+            LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+
+        _binding.collectionsRecyclerView.adapter = CollectionsListAdapter()
+        _binding.collectionsRecyclerView.addItemDecoration(
+            SpacesItemDecoration(
+                topFirst = this.requireContext().dpToPixel(24f).toInt(),
+                leftFirst = this.requireContext().dpToPixel(16f).toInt(),
+                bottomFirst = this.requireContext().dpToPixel(32f).toInt(),
+                left = this.requireContext().dpToPixel(
+                    22f
+                ).toInt(),
+                right = this.requireContext().dpToPixel(
+                    22f
+                ).toInt(),
+                bottom = this.requireContext().dpToPixel(20f).toInt()
+            )
+        )
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/nastirlex/cinema/presentation/main/collections/CollectionsListAdapter.kt
@@ -0,0 +1,32 @@
+package com.nastirlex.cinema.presentation.main.collections
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nastirlex.cinema.R
+import com.nastirlex.cinema.databinding.ItemCollectionsListBinding
+
+class CollectionsListAdapter :
+    RecyclerView.Adapter<CollectionsListAdapter.CollectionsListViewHolder>() {
+    class CollectionsListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val viewBinding = ItemCollectionsListBinding.bind(view)
+
+        fun bind() {}
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionsListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_collections_list, parent, false)
+
+        return CollectionsListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onBindViewHolder(holder: CollectionsListViewHolder, position: Int) {
+        holder.bind()
+    }
+}
