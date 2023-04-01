55268508e8a44f8e35555b283b1e060bf05f464a
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-01 14:54:09 +0700

    Rename collectionActivity -> collectionInfoActivity

--- app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection_info/CollectionInfoActivity.kt
+++ app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection_info/CollectionInfoActivity.kt
@@ -1,12 +1,12 @@
-package com.nastirlex.cinema.presentation.main.collections.collection
+package com.nastirlex.cinema.presentation.main.collections.collection_info
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nastirlex.cinema.R
 
-class CollectionActivity : AppCompatActivity() {
+class CollectionInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_collection)
+        setContentView(R.layout.activity_collection_info)
     }
 }
