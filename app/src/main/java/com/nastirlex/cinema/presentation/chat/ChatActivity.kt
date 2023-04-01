d9c92893a9c5fccc9340a3cb0a5958f2ab20599b
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-01 14:43:19 +0700

    Empty chatActivity and layout

---
+++ app/src/main/java/com/nastirlex/cinema/presentation/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.nastirlex.cinema.presentation.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nastirlex.cinema.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
