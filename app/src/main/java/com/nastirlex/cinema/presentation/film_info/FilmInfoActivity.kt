2e771cdcffa7394594a0b8e40e77ff0558aa8b60
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-01 14:36:46 +0700

    Empty filmInfo activity and layout

---
+++ app/src/main/java/com/nastirlex/cinema/presentation/film_info/FilmInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.nastirlex.cinema.presentation.film_info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nastirlex.cinema.R
+
+class FilmInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_info)
+    }
+}
