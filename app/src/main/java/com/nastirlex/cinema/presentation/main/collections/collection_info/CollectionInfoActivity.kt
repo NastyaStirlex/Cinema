1f6eab6ea38dec899e8560adb339af36bf065872
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-02 00:06:43 +0700

    :lipstick: collection
    collection films

--- app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection_info/CollectionInfoActivity.kt
+++ app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection_info/CollectionInfoActivity.kt
@@ -1,12 +1,24 @@
 package com.nastirlex.cinema.presentation.main.collections.collection_info
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nastirlex.cinema.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nastirlex.cinema.databinding.ActivityCollectionInfoBinding
+import com.nastirlex.cinema.utils.CollectionFilmsItemListSpacesItemDecoration
+import com.nastirlex.cinema.utils.dpToPixel
 
 class CollectionInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCollectionInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCollectionInfoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_collection_info)
+        setContentView(binding.root)
+
+        binding.collectionFilmsRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.collectionFilmsRecyclerView.adapter = CollectionFilmsListAdapter()
+        binding.collectionFilmsRecyclerView.addItemDecoration(
+            CollectionFilmsItemListSpacesItemDecoration(bottom = this.dpToPixel(16f).toInt(), start = 0, end = 0)
+        )
     }
 }
