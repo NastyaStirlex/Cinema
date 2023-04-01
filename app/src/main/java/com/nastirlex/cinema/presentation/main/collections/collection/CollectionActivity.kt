70f2e1ccf6973cc20e0a05948fe8b8226a69f8fd
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-01 14:31:28 +0700

    Empty collection activity and layout

---
+++ app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection/CollectionActivity.kt
@@ -0,0 +1,12 @@
+package com.nastirlex.cinema.presentation.main.collections.collection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nastirlex.cinema.R
+
+class CollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}
