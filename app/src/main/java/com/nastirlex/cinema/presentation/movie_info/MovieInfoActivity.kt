a469d7fd08eaaac76bdfb5803d4fee5e67e40410
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-01 14:39:04 +0700

    Rename filmInfo -> movieInfo

--- app/src/main/java/com/nastirlex/cinema/presentation/movie_info/MovieInfoActivity.kt
+++ app/src/main/java/com/nastirlex/cinema/presentation/movie_info/MovieInfoActivity.kt
@@ -1,12 +1,12 @@
-package com.nastirlex.cinema.presentation.film_info
+package com.nastirlex.cinema.presentation.movie_info
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nastirlex.cinema.R
 
-class FilmInfoActivity : AppCompatActivity() {
+class MovieInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_film_info)
+        setContentView(R.layout.activity_movie_info)
     }
 }
