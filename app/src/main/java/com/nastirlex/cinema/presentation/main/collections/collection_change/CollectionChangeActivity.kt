687c7057cabad384e93ab9e67c970e8e76b125a0
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2023-04-02 14:02:45 +0700

    :lipstick: layout(collecton change)
    to add: pass as parameter name of collection

--- app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection_change/CollectionChangeActivity.kt
+++ app/src/main/java/com/nastirlex/cinema/presentation/main/collections/collection_change/CollectionChangeActivity.kt
@@ -3,10 +3,14 @@ package com.nastirlex.cinema.presentation.main.collections.collection_change
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nastirlex.cinema.R
+import com.nastirlex.cinema.databinding.ActivityCollectionChangeBinding
 
 class CollectionChangeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCollectionChangeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_collection_change)
+
+        binding = ActivityCollectionChangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
